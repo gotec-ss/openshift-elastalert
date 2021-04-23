8a17b7bc8af0e7e488b65ff17b9dbb95fbc29549
Patrick McMahon <pmcmahon@crossvale.com>
2021-04-23 11:16:41 +0100

    disable index creation

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@
 # --debug option will result in alerts NOT being sent
 
 cd $ELASTALERT_HOME && \
-    elastalert-create-index --config $ELASTALERT_HOME/config/config.yaml && \
     python -m elastalert.elastalert --config $ELASTALERT_HOME/config/config.yaml --verbose --start $(date --iso-8601)T00:00:00 
+
+
+    ##elastalert-create-index --config $ELASTALERT_HOME/config/config.yaml && \
